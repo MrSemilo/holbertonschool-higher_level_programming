f69ee8492d55605d6cb580d48c2cf0162586c83d
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-04-27 01:55:54 +0000

    Python10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,17 +6,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *var1;
+	listint_t *var1, *var2;
 
-	var1 = list;
-	if (!list)
-		return (0);
+	var1 = var2 = list;
 
-	while (list && var2 && var1->next)
+
+	while (var1 && var2 && var1->next)
 	{
-		var1 = var1->next->next;
-		list = list->next;
-		if (list == var1)
+		var1 = var1->next;
+		var2 = var2->next->next;
+		if (var1 == var2)
 			return (1);
 	}
 	return (0);
