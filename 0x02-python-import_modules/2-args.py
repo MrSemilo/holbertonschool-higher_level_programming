d9c349f5585b03ae5f27aa4ede69902b02c6bf5b
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-04-29 00:45:32 +0000

    Python argumet

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+
+    c = len(argv) - 1
+    a = len(argv)
+    if j == 0:
+        print("{} arguments.".format(c))
+
+    else:
+        if c == 1:
+            print("{} argument:".format(c))
+        else:
+            print("{} arguments:".format(c))
+
+        for i in range(1, a):
+            print("{:d}: {:s}".format(i, argv[i]))
