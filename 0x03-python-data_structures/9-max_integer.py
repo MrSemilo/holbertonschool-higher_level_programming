6aea9a893994af2378c5945055f176d8f61b0d3c
samilo-del <camilovelenciapadilla@gmail.com>
2021-05-06 03:58:59 +0000

    9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     stri = None
+
     if sentence:
         stri = sentence[0]
+
     len_sentence = len(sentence)
+
     return (len_sentence, stri)
