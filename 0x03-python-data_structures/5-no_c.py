246bc9852ab1eebe48cf477833aa5be000892058
samilo-del <camilovelenciapadilla@gmail.com>
2021-05-06 01:58:33 +0000

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     characters = "Cc"
 
-    my_string = ''.join(x for x in my_string if x not in characters)
+    new_string = ''.join(x for x in my_string if x not in characters)
 
-    return my_string
+    return new_string
