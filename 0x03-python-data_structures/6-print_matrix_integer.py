72e139b75555ec3d9da3829c02b1bb3fd22e7006
samilo-del <camilovelenciapadilla@gmail.com>
2021-05-06 02:35:12 +0000

    7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for x in range(len(i)):
+            print("{:d}".format(i[x]), end="")
+
+            if x < len(i) - 1:
+                print(" ", end="")
+
+        print("")
