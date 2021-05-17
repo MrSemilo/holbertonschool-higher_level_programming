cae20184b59a26761d5681c7a4bcbd61d91e8184
samilo-del <camilovelenciapadilla@gmail.com>
2021-05-17 23:48:11 +0000

    add pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/user/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -7,5 +8,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError:
         None
+
     print()
     return count
