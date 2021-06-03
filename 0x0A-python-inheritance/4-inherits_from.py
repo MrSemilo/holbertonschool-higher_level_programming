b30a15828572f80c038840d3a8e3ffdc42100f5b
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-03 02:54:13 +0000

    add point 4 pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module function inherits_from"""
 
+
 def inherits_from(obj, a_class):
     """ def inherits_from(obj, a_class): """
     if type(obj) is not a_class and isinstance(obj, a_class):
