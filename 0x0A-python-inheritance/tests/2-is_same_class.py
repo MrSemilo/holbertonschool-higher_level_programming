8febe46ce124ea654e57ac5649143fc74845d8f7
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-03 02:00:21 +0000

    add point 2

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module function is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """ def is_same_class(obj, a_class):"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
