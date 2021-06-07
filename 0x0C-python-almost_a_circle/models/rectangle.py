06516bf5d88f8e4b51a5c3ddef109ede3802c531
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-07 23:14:54 +0000

    add point 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,15 @@ class Rectangle(Base):
         i = "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
         return i
+
+    def update(self, *args):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
