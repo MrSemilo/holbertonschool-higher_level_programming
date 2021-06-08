3b267cd97956cc85db307c4eaf635b29618e343c
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-08 21:13:25 +0000

    add func update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,3 +126,10 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """to_dictionary"""
+
+        my_dict = {'id': self.id, 'width': self.__width,
+                   'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return my_dict
