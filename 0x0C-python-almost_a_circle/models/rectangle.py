0d555d96e7ec40e5960cc34d6ffe61fb8064fdd0
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-08 20:29:00 +0000

    add pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -40,7 +40,7 @@ class Rectangle(Base):
         if not type(value) is int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
