ea571c725e07733fed2b58a9592b334fe5daa9cd
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-08 20:38:38 +0000

    add pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,8 @@ from models.base import Base
 class Rectangle(Base):
     """def init"""
 
+    print_symbol = '#'
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
