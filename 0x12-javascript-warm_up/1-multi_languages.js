c87e03fd7598f13afd6323c5454ba00c56423861
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-02 20:24:27 +0000

    add point 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-let myVa = "C is fun"; let myV = "\nPython is cool"; let my = "\nJavaScript is amazing"; console.log(myVa,myV,my)
+const myVa = "C is fun"; const myV = "\nPython is cool"; const my = "\nJavaScript is amazing"; console.log(myVa,myV,my)
