3ea464e414a7f2f2ee7ea2991562f14d4119ae61
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-03 22:41:24 +0000

    add point 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Square Class
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
