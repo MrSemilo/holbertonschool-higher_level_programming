4b23b94b6e47ede6a36eab9b8ca55758d5bedc63
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-03 00:50:56 +0000

    add point 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-const myVar = parseInt(process.argv[2]); if (isNaN(myVar)) cosole.log('Not a number'); else console.log(`My number: ${myVar}`);
+const myVar = parseInt(process.argv[2]); if (isNaN(myVar)) console.log('Not a number'); else console.log(`My number: ${myVar}`);
