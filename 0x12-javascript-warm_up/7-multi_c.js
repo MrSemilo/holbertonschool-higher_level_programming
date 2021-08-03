61c5e5ec152c46968a48b4c55168db2231c54c25
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-03 02:42:11 +0000

    add point 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,7 @@ const myVar = 'C is fun';
 const myArg = parseInt(process.argv[2]);
 if (isNaN(myArg)) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   for (let i = 0; i < myArg; i++) {
     console.log(myVar);
   }
