9091715cd582133a6ec10160817507b690094142
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-03 00:19:13 +0000

    add file 3

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-
-const myVar = process.argv[2];
-const myArgv = process.argv[3];
-console.log(myVar + " is " + myArgv);
+const myVar = process.argv[2]; const myArgv = process.argv[3]; console.log(myVar + " is " + myArgv);
