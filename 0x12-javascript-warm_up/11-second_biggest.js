473a1dc8045b8f6ea373a9efc8344636a7d774b4
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-03 03:25:57 +0000

    add point 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// second_biggest
+const myVar = process.argv;
+const array = [];
+let count;
+for (count = 2; count < myVar.length; count++) {
+  array.push(parseInt(myVar[count]));
+}
+array.splice(array.indexOf(Math.max(...array)), 1);
+if (array.length === 0) {
+  console.log(0);
+} else {
+  console.log(Math.max(...array));
+}
