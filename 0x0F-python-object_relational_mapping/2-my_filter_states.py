27e75f8c2444f6d258730659b6bf93139aa16a7b
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-09 03:41:11 +0000

    add point 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,17 +2,16 @@
 """takes in an argument and displays all values in the states"""
 
 if __name__ == '__main__':
-    """main"""
 
     import MySQLdb
-    from sys import argv
+    import sys
 
-    n = MySQLdb.connect(host="localhost", port=3306,
-                        user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    u = conn.cursor()
-    u.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(argv[4]))
-    _rows = cur.fetchall()
-    for row in _rows:
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
