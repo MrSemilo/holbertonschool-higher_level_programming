04032a0e15bb58d916fa28a11026f78e0e04844b
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-09 03:56:14 +0000

    add point 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,17 +2,18 @@
 
 """ write a script that takes in arguments and displays
     all values in the states table of hbtn_0e_0_usa """
+
 if __name__ == '__main__':
 
     import MySQLdb
-    from sys import argv
+    import sys
 
-    n = MySQLdb.connect(host='localhost', port=3306,
-                        user=argv[1], passwd=argv[2], db=argv[3])
+    r = MySQLdb.connect(host='localhost', port=3306,
+                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    u = conn.cursor()
+    u = r.cursor()
     u.execute("SELECT * FROM states WHERE name=%s\
-              ORDER BY states.id ASC", (argv[4],))
-    _rows = u.fetchall()
+              ORDER BY states.id ASC", (sys.argv[4],))
+    _rows = cur.fetchall()
     for row in _rows:
         print(row)
