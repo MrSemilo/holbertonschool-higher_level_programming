1691fa87c6b61cc1a5209db12a48f547fd1254a5
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-09 03:25:41 +0000

    add point 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""tsts 2"""
+
+if __name__ == '__main__':
+    """main"""
+
+    import MySQLdb
+    from sys import argv
+
+    n = MySQLdb.connect(host="localhost", port=3306,
+                        user=argv[1], passwd=argv[2], db=argv[3])
+
+    u = n.cursor()
+    u.execute("SELECT * FROM states WHERE name LIKE BINARY
+              'N%' ORDER BY states.id ASC")
+    _rows = u.fetchall()
+    for row in _rows:
+        print(row)
