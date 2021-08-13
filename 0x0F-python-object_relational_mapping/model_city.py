ac12045ed5f40068769d9045b58219f42a7d9e06
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-13 19:38:05 +0000

    add point

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+class city
+"""
+
+
+from model_state import State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    This class is linked to the cities table.
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey(State.id))
