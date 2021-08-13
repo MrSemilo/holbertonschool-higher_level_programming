52ffcef767bdaa3e3f7f097dabdd72f32d8de0fb
samilo-del <77862328+samilo-del@users.noreply.github.com>
2021-08-13 11:21:44 -0500

    Create 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""__name__"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name="Louisiana")
+
+    session.add(louisiana)
+    session.commit()
+
+    print("{}".format(louisiana.id))
