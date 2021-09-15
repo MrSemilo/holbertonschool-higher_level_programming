5ee7177f9e88c4892c77907c0d64aa3cd3afc037
samilo-del <camilovelenciapadilla@gmail.com>
2021-09-15 01:41:41 +0000

    add point 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  script that reads and prints the content of a file.
+
+const argv = process.argv[2];
+const request = require('request');
+
+request(argv, function (err, result, body) {
+  console.log('code: ' + result.statusCode);
+  if (err) {
+    console.log('error: ', err);
+  }
+});
