5f6f580916c66063755f26bbbbbe33ca77c697fd
samilo-del <camilovelenciapadilla@gmail.com>
2021-09-15 01:12:03 +0000

    add point 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-//script that reads and prints the content of a file.
+//  script that reads and prints the content of a file.
 
 const argv = process.argv[2];
-const  fs = require('fs');
+const fs = require('fs');
 
 fs.readFile(argv, 'UTF-8', function (error, descri) {
   if (error) {
